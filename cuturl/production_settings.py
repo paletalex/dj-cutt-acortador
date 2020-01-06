7b469aec6b98547de150c1f478b9a72b961c4af8
paletalex <misopitas@gmail.com>
2020-01-06 11:22:18 -0600

    db

--- cuturl/production_settings.py
+++ cuturl/production_settings.py
@@ -2,15 +2,19 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['cuttt.herokuapp.com']
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dcovo3j91bdgif',
+        'USER': 'puhhzlkwgktqzm',
+        'PASSWORD': '6e06d660a342f62993a8802047fcc6b4d168773eac2b3dbd97320aab30017e82',
+        'HOST': 'ec2-174-129-33-13.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
